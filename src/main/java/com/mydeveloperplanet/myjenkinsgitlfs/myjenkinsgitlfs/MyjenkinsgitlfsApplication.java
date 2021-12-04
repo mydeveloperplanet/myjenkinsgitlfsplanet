feef7c24c14ac50de0a7bf126a494656d4741fae
mydeveloperplanet <gunter@mydeveloperplanet.com>
2021-12-04 16:28:31 +0100

    initial commit

---
+++ src/main/java/com/mydeveloperplanet/myjenkinsgitlfs/myjenkinsgitlfs/MyjenkinsgitlfsApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myjenkinsgitlfs.myjenkinsgitlfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjenkinsgitlfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjenkinsgitlfsApplication.class, args);
+	}
+
+}
